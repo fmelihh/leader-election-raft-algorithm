00a45b5aa134561073da89f68fb2b9940d562ccf
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-04-27 00:58:00 +0300

    feat: init

--- src/raft_algorithm/api/router/raft_node.py
+++ src/raft_algorithm/api/router/raft_node.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
 
+from ...schemas.enums import StateEnum
+
 
 class RaftNodeRouter:
     def __init__(self, node_id: str, peers: list[str]):
-        self.node_id = node_id
         self.peers = peers
+        self.node_id = node_id
+        self.state = StateEnum.FOLLOWER
+
+        self.log = []
 
         self.router = APIRouter()
         self.router.add_api_route("/hello", self.hello, methods=["GET"])
+
 
     def hello(self):
         pass
