41ef41e861178cdb20bf7d72feccb5602a8ee15d
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-04-27 16:45:06 +0300

    feat: election loop on fastapi lifespan events

--- node_1.py
+++ node_1.py
@@ -1,9 +1,19 @@
 import uvicorn
+import asyncio
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from src.raft_algorithm.api import raft_router
+from src.raft_algorithm.services import get_raft_node_service
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(get_raft_node_service().election_loop())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(raft_router, prefix="/raft", tags=["raft"])
 
--- node_3.py
+++ node_3.py
@@ -1,12 +1,21 @@
 import uvicorn
+import asyncio
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from src.raft_algorithm.api import raft_router
+from src.raft_algorithm.services import get_raft_node_service
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(get_raft_node_service().election_loop())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(raft_router, prefix="/raft", tags=["raft"])
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
