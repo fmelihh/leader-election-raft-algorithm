f6bb30612e8cba45f51d9bea36dbf6a3d8db03af
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-04-27 15:25:57 +0300

    feat: router structure

--- src/raft_algorithm/services/raft.py
+++ src/raft_algorithm/services/raft.py
@@ -1,9 +1,30 @@
-from .raft_node import RaftNode
+import os
+import time
+import httpx
+import random
+import asyncio
+from loguru import logger
+
+from ..schemas.enums import StateEnum
 
 
-class RaftNodeService(RaftNode):
+class RaftNodeService:
     def __init__(self):
-        super().__init__()
+        self._id = os.getenv("RAFT_NODE_ID")
+        self._peers = [
+            f"http://localhost:{port}/raft"
+            for port in os.getenv("RAFT_NODE_PEERS").split(",")
+        ]
+
+        self.votes = 0
+        self.voted_for = None
+        self.current_term = 0
+        self.state = StateEnum.FOLLOWER
+        self.last_heartbeat = time.time()
+        self.election_timeout = random.uniform(1.0, 3.0)
+
+        self._logs = []
+        self._db = set()
 
     async def set_item(self, key: str, value: str):
         pass
@@ -13,6 +34,55 @@ class RaftNodeService(RaftNode):
 
     async def get_item(self):
         pass
+
+    async def election_loop(self):
+        while True:
+            await asyncio.sleep(0.5)
+            now = asyncio.get_event_loop().time()
+
+            if self.state == StateEnum.FOLLOWER and (
+                now - self.last_heartbeat > self.election_timeout
+            ):
+                await self._start_election()
+
+    async def _start_election(self):
+        self.state = StateEnum.CANDIDATE
+        self.current_term += 1
+        self.voted_for = self._id
+
+        votes = 1
+        logger.info(f"{self._id} Starting election for term {self.current_term}")
+        async with httpx.AsyncClient() as client:
+            for peer in self._peers:
+                try:
+                    response = await client.post(
+                        f"{peer}/request-vote",
+                        json={"term": self.current_term, "candidate": self._id},
+                    )
+                    result = response.json()
+                    if result.get("vote_granted", False):
+                        votes += 1
+                except Exception as e:
+                    logger.exception(f"Error contacting {peer}", e)
+
+        if votes > (len(self._peers) + 1) // 2:
+            self.state = StateEnum.LEADER
+            logger.info(f"{self._id} Elected Leader for term {self.current_term}")
+            asyncio.create_task(self.leader_heartbeat())
+
+    async def leader_heartbeat(self):
+        while self.state == StateEnum.LEADER:
+            async with httpx.AsyncClient() as client:
+                for peer in self._peers:
+                    try:
+                        await client.post(
+                            f"{peer}/append-entries",
+                            json={"term": self.current_term, "leader_id": self._id},
+                        )
+                    except Exception as e:
+                        logger.exception(f"Error sending heartbeat to {peer}", e)
+
+                await asyncio.sleep(1.0)
 
 
 _raft_node_service = None
